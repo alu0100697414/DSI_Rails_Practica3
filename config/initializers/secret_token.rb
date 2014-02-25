3bf5f2226cc5491f106e820df3ec880ee50a4583
Jose <alu0100697414@ull.edu.es>
2014-02-25 10:40:53 +0000

    Creada la estructura principal de Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '0b142743e0524eb0ab3b57355fac52c9951d42aa33bf84dbc54cfec84f10e6c834db51440ccdf605c6f06848a5fbb3afc26f45b3c66bd5353bf606958c1e199d'
